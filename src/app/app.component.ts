2bfe340d15769c347637d32a5cac4769ff4553b5
Suresh Karbhari Date <sureshdat@cybage.com>
2023-12-11 19:37:44 +0530

    searching_sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'searching_sorting';
+  originalEmployees:Employee[]=[
+    new Employee(101,"John",5000),
+    new Employee(102,"smith",12000),
+    new Employee(103,"Nick",6000)
+  ]
+
+  employees:Employee[]=[];
+  
+  constructor(){
+    this.employees=this.originalEmployees;
+  }
+  
+  str:string="";
+  sortcolumn="empId";
+  order=1;
+
+
+  onSearchClick(){
+    this.employees=this.originalEmployees.filter((emp)=>{
+      return
+      emp.empName.toLowerCase().indexOf(this.str.toLowerCase())>=0;
+    })
+  }
+
+
+  onSortClick(){
+    this.employees=this.originalEmployees.sort((emp1:Employee,emp2:Employee):any=>
+    {
+      var n=0;
+      if(this.sortcolumn=="empId"){
+        return
+        (emp1[this.sortcolumn]-emp2[this.sortcolumn])*this.order;
+      }
+      else if(this.sortcolumn=="empName"){
+        (emp1[this.sortcolumn].charCodeAt(0)-(emp2[this.sortcolumn].charCodeAt(0)*this.order,
+      }
+      else{
+        return
+        (emp1[this.sortcolumn]-emp2[this.sortcolumn])*this.order;
+      }
+    })
+  }
 }
---
+++ src/app/employee.ts
@@ -0,0 +1,12 @@
+export class Employee {
+    empId:number;
+    empName:string;
+    salary:number;
+
+    constructor(empId:number,empName:string,salary:number)
+    {
+        this.empId=empId;
+        this.empName=empName;
+        this.salary=salary;
+    }
+}
