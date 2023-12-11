53ccb0f914efa905c78b30d936adf085d4b5e1b3
Suresh Karbhari Date <sureshdat@cybage.com>
2023-12-11 19:29:10 +0530

    data_binding added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data_binding';
+  fname:string="suresh";
+  lname:string="date";
+  age:number=27;
+  gender:string="male";
+  country:string="Uk"
+  isEmployed:boolean=true;
+  
+  
+  handSubmitButton(){
+    this.fname="pooja";
+    this.lname="date";
+    this.age=25;
+    this.gender="female";
+    this.country="Us";
+    this.isEmployed=false;
+}
 }
