72fd36b5b9ad8ba446737a2e72893745b7e64754
Suresh Karbhari Date <sureshdat@cybage.com>
2023-12-11 19:32:04 +0530

    directive branch added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,160 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives';
+ /*//style-directives
+ marks:number=70;
+ mycolor:string="";
+
+ constructor(){
+  if(this.marks>=35){
+   this.mycolor="green";
+  }
+  else{
+    this.mycolor="red";
+  }*/
+
+ /* //ng class directives
+   marks:number=80;
+   mycolor:string="";
+
+   constructor(){
+    if(this.marks>35){
+      this.mycolor="class1";
 }
+    else{
+      this.mycolor="class2";
+    }
+   }*/
+  
+   
+   /*//ngif directive 
+   marks:number=80;
+   b:boolean;
+
+   constructor(){
+    if(this.marks>=35){
+      this.b=true;
+    }
+    else{
+      this.b=false;
+    }
+   }*/
+
+   /*//ngIf and else directive
+   marks:number=20;
+   b:boolean;
+
+   constructor(){
+    if(this.marks>=35){
+      this.b=true;
+    }
+    else{
+      this.b=false;
+    }
+   }*/
+ 
+   //ng-switch directive
+  //  country:string="null";
+
+  //ngFor directive
+  //cities:string[]=["new delhi","mumbai","nagpur"];
+
+  //ngFor with object array
+  employees:Employee[]=[
+    new Employee(101,"john",5000),
+    new Employee(102,"smith",5000),
+    new Employee(103,"Nick",6900)
+  
+  ]
+  newemployee:Employee=new Employee(0,"",0);
+
+  onInsertClick(){
+    this.employees.push(new Employee(this.newemployee.empId,this.newemployee.empName,this.newemployee.salary));
+    this.newemployee.empId=0;
+    this.newemployee.empName="";
+    this.newemployee.salary=0;
+  }
+
+  onDeleteClick(index:number){
+  if(confirm("Are u sure to delete this emp?")){
+    this.employees.splice(index,1);
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/app/employee.ts
@@ -0,0 +1,15 @@
+export class Employee {
+
+    empId:number;
+    empName:string;
+    salary:number;
+
+    constructor(empId:number,empName:string,salary:number){
+        this.empId=empId;
+        this.empName=empName;
+        this.salary=salary;
+
+        
+
+    }
+}
