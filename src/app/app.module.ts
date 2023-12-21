9d4e29f8ad1ac087f390307d54ec1ba88a80a3c5
Suresh Karbhari Date <sureshdat@cybage.com>
2023-12-21 15:45:46 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
+import {  RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+var MyRoutes:Routes=[
+  {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"service",component:ServiceComponent},
+  {path:"contact",component:ContactComponent}
+]
+
+var myRoutes2=RouterModule.forRoot(MyRoutes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    myRoutes2
   ],
   providers: [],
   bootstrap: [AppComponent]
